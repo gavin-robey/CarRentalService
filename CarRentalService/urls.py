da1b3a9293aba5cee37e8be3462627d9f6695c06
jkamino <jkamino@live.com>
2023-02-13 12:59:40 -0700

    added user login page, logout page, and display username to screen on homepage

--- CarRentalService/urls.py
+++ CarRentalService/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path('rental/', include('rental.urls')),
     path('', user_views.home, name='home'),
     path('register/', user_views.register, name='register'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
