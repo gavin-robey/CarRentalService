845aa2f145283283768fe7cf87eee1e4d8bbe360
jkamino <jkamino@live.com>
2023-02-27 17:12:11 -0700

    finished profile model, auto generate on user creation

--- users/models.py
+++ users/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
+    role = models.TextField(default='user')
+    moneyBalance = models.IntegerField(default=0)
+    hoursWorked = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.user)
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
+
     
