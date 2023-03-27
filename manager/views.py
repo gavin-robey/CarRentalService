d348175349889c1cc326d93586362b00508316a0
jkamino <jkamino@live.com>
2023-03-26 20:44:32 -0600

    display users working

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 def manager(request):
-    context = {'name': 'Bob'}
+    all_users= get_user_model().objects.all()
+    
+    context= {'allusers': all_users}
     return render(request, 'manager.html', context)
